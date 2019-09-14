e736b24789fbdc5fd54806f1d29c154442c7875a
Jason Loomis <jason@jrloomis.com>
2019-09-14 14:54:20 -0400

    added styling for dark mode, some simple styling for player display

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDarkMode } from "../Hooks/useDarkMode";
+import { Button, Icon, Segment } from "semantic-ui-react";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useDarkMode(false);
@@ -8,12 +9,16 @@ const Navbar = () => {
     setDarkMode(!darkMode);
   };
   return (
-    <nav>
-      <h1>Women's World Cup Players Ranked by Google Trends</h1>
-      <div onClick={toggleMode} className={darkMode ? "toggle toggled" : "toggle"}>
-        Darkmode
-      </div>
-    </nav>
+      <header className="navbar">
+        <div className="blank"></div>
+        <div className="header">
+          <h1 as="h1">Women's World Cup Players</h1>
+          <div>(Ranked by Google Trends)</div>
+        </div>
+        <div className="darkModeButton">
+          <Button circular icon={"lightbulb outline"} toggle onClick={toggleMode} className={darkMode ? "toggle toggled" : "toggle"} />
+        </div>
+      </header>
   );
 };
 
--- client/src/Components/Player.js
+++ client/src/Components/Player.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Player = ({ country, name, searches }) => {
   return (
-    <section>
+    <section className="playerCard">
       <h2 data-testid={name}>{name}</h2>
       <p data-testid={country}>{country}</p>
       <p data-testid={searches}>{searches}</p>
