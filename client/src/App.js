6f4c1af5c9c2eebfd5d41cb688fb78906542d787
Jason Loomis <jason@jrloomis.com>
2019-09-14 12:28:31 -0400

    fetching data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <div>Div!</div>
-    </div>
-  );
+class App extends React.Component {
+  state = { data: [] };
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(resolve => console.log(resolve))
+      .catch(error => console.log("fetched bad things --> ", error));
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Div!</div>
+      </div>
+    );
+  }
 }
 
 export default App;
